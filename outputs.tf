4072bb4a24f8f4491d81d8fed9c298ed1b72b9f1
Atishay <atishay2024@outlook.com>
2024-11-17 11:32:20 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.vpc.id
+}
+
+output "subnet_id" {
+  description = "The ID of the Subnet"
+  value       = aws_subnet.subnet.id
+}
